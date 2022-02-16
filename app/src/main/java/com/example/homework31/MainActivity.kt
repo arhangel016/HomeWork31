01de97537d7f2f0776cd92abb77e6571538a9a0a
Lenovo <aliayrsulaimanov@gmail.com>
2022-02-16 18:48:52 +0600

    Initial commit

---
+++ app/src/main/java/com/example/homework31/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.homework31
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.homework31.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
